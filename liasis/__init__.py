3f1a78ac26270a7d2e183c570cb37e2cd65bbf8e
Johnny Well <johnny.santos@snowmanlabs.com>
2018-06-11 08:27:41 -0300

    Expose core packages at library level

---
+++ liasis/__init__.py
@@ -0,0 +1,4 @@
+from .core import *
+
+
+__all__ = ['application', 'domain', 'infrastructure']
---
+++ liasis/core/__init__.py
@@ -0,0 +1,6 @@
+from . import application
+from . import domain
+from . import infrastructure
+
+
+__all__ = ['application', 'domain', 'infrastructure']
