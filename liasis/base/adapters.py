8696b5b467852f2ea8348963e93b151b92fc4fca
Johnny <johnny.w.santos@gmail.com>
2019-06-09 21:31:47 -0300

    Implements basic jso conversion to BaseAPIAdapter

--- liasis/base/adapters.py
+++ liasis/base/adapters.py
@@ -1,7 +1,10 @@
 from liasis.core.protocols import Adapter
+import json
 
 
 class BaseAPIAdapter(Adapter):
 
-    def __init__(self, *args, **kwargs) -> None:
-        raise NotImplementedError
+    def __call__(self, response, *args, **kwargs) -> str:
+        if response.error:
+            return json.dumps(response.error.__dict__)
+        return json.dumps(response)
