9c4c76eec23f0b4b1459600371d2eb39bb10dd2f
Wallace Silva <aliga12@yahoo.com.br>
2019-07-25 14:55:58 -0300

    raise InfrastructureError when response is not from protocol on adapter
    when call adapter and the responde paramater is not a ResponseProtocol
    need to raise an error to use right response.
    
    default error is an Infrastructure erro but we can change this in
    future.
    
    error found on trying make tests to adapter
    
    Issue:  #2

--- liasis/base/adapters.py
+++ liasis/base/adapters.py
@@ -1,10 +1,16 @@
 from liasis.core.protocols import AdapterProtocol
+from liasis.core.errors.infrastructure import InfrastructureError
+from liasis.core.protocols import ResponseProtocol
 import json
 
 
 class BaseAPIAdapter(AdapterProtocol):
 
     def __call__(self, response, *args, **kwargs) -> str:
+
+        if not isinstance(response, ResponseProtocol):
+            raise InfrastructureError(code=1, title='Adapter Error', message='Error on passing Response to Adapter '+__name__)
+
         if response.error:
             return json.dumps(response.error.__dict__)
         return json.dumps(response.data)
